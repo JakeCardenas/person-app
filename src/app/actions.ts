1b95612eb9dd889f6a3d7fb4b7c91016537b06de
Jake <marijakee@gmail.com>
2026-04-22 15:25:06 +0800

    feat: complete person app with CRUD

---
+++ src/app/actions.ts
@@ -0,0 +1,21 @@
+'use server'
+
+import { prisma } from '@/lib/prisma'
+import { revalidatePath } from 'next/cache'
+
+export async function createPerson(formData: FormData) {
+  await prisma.person.create({
+    data: {
+      firstName: formData.get('firstName') as string,
+      lastName: formData.get('lastName') as string,
+      email: formData.get('email') as string,
+      age: parseInt(formData.get('age') as string),
+    },
+  })
+  revalidatePath('/')
+}
+
+export async function deletePerson(id: number) {
+  await prisma.person.delete({ where: { id } })
+  revalidatePath('/')
+}
